4002aa39c860afc8b2d7629dd7420ad929ff063f
hailinhbui <hailinh.bui@idealo.de>
2021-10-29 16:35:05 +0200

    add comments

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_queries(cur):
       and lower(query) not like '%from system.%'
       -- Helps ignore the prepared statements (e.g. EXECUTE, ALLOCATE, DEALLOCATE, etc)
       and regexp_like(lower(query), '^(with|select)')
-      and lower(query) like '%fact_checkouts%'
+      and lower(query) like '%fact_leadouts%'
     limit 10000
     ''')
     records = cur.fetchall()
@@ -53,8 +53,6 @@ def cleanQueries(queries):
         text_lower = text_nonumbers.lower()
         # tokenziation a string into a list of tokens
         tokens = word_tokenize(text_lower)
-        # take only unique values from that list of tokens
-        # unique_tokens = list(set(tokens))
         # remove sql syntax keywords
         for i in sql_syntax:
             try:
@@ -64,13 +62,14 @@ def cleanQueries(queries):
         cleaned_lists.append(tokens)
     for list in cleaned_lists:
         merged_list += list
-    return merged_list
-
-def sorting_occurrencies(merged_list, table_columns):
+    # Count how often a token was found in the list
     counter = Counter(merged_list)
     counter_dict = dict(counter)  # convert Counter to Dict
+    return counter_dict
+
+def sorting_occurrencies(counter_dict, table_columns):
     # Using dict()
-    # Extracting specifix keys from dictionary: selecting the values of fields in leadouts_fields
+    # Extracting specific keys from dictionary: selecting the values of fields in leadouts_fields
     occurencies = dict((k, counter_dict[k]) for k in table_columns if k in counter_dict)
     sorted_keys = sorted(occurencies, key=occurencies.get)
     sorted_occurencies = {}
@@ -93,15 +92,14 @@ def get_table_colums(cur, table_name):
 if __name__ == '__main__':
     cursor = connect_to_presto()
     query_records = get_queries(cursor)
-    table_name = 'fact_checkouts'
+    table_name = 'fact_leadouts'
     table_columns = get_table_colums(cursor,table_name)
     # convert the field query into a list
     queries = query_records["query"].values.tolist()
-    merged_list = cleanQueries(queries)
+    counter_dict = cleanQueries(queries)
     #showing the unused fields in the table - which are normally empty
     #get_unused_fields = set(leadouts_fields) - set(merged_list)
     # print(get_unused_fields)
-    sorted_occurencies = sorting_occurrencies(merged_list, table_columns)
+    sorted_occurencies = sorting_occurrencies(counter_dict, table_columns)
     for key, value in sorted_occurencies.items():
         print(key, ' : ', value)
-
